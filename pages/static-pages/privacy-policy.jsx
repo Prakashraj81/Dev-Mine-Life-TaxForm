e36538335b84fc20b91109ce8bd3e57ce6d904ab
Prakashraj81 <prakash.raj@g-japan.com>
2023-09-01 12:17:32 +0530

    privacy-policy-page

---
+++ pages/static-pages/privacy-policy.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from './../../components/header';
+import Footer from './../../components/footer';
+
+export default function PrivacyPolicy() {
+    return
+    (
+        <>
+            <Header />
+            <div className="privacy-policy-wrapper">
+                <div>
+                    
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
