777cc0ca2c2238ca2aef243e1f7d5756a99d812d
Prakashraj81 <prakash.raj@g-japan.com>
2023-11-24 10:32:06 +0530

    correction changes-11

--- pages/declaration-printing/debt/debt-add.jsx
+++ pages/declaration-printing/debt/debt-add.jsx
@@ -429,13 +429,15 @@ export default function DebtAdd() {
                     </div>
                     
 
-                    <div className="w-full block lg:flex xl:flex 2xl:flex justify-evenly items-center">
-                        <BackButton />
-                        <SubmitButton onSubmit={onSubmit} isSumbitDisabled={isSumbitDisabled} />
-                    </div>
-                    <div className="heading text-center pt-8">
-                        <h5 className="text-sm text-black tracking-2 font-medium">必須入力項目があります。</h5>
-                    </div>
+                    <div className="Total-property-section py-10 lg:py-20 xl:py-20 2xl:py-20 px-20 lg:px-36 xl:px-36 2xl:px-36 mx-auto w-full lg:max-w-screen-md xl:max-w-screen-md 2xl:max-w-screen-md">
+                        <div className="w-full block lg:flex xl:flex 2xl:flex justify-evenly items-center">
+                            <BackButton />
+                            <SubmitButton onSubmit={onSubmit} isSumbitDisabled={isSumbitDisabled} />
+                        </div>
+                        <div className="heading text-center pt-8">
+                            <h5 className="text-sm text-black tracking-2 font-medium">必須入力項目があります。</h5>
+                        </div>
+                        </div>        
                 </form>
 
 
