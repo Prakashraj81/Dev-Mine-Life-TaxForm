f75bc6d5872fb1d083477539de2cbc0c8e50358c
Prakashraj81 <prakash.raj@g-japan.com>
2024-03-08 11:29:12 +0530

    CSS added
    CSS added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
-'use strict'; 
-const fs = require('fs');
-const express = require('express');
-const routes = express.Router();
-const apiURL = "https://minelife-api.azurewebsites.net/";
+// 'use strict'; 
+// const fs = require('fs');
+// const express = require('express');
+// const routes = express.Router();
+// const apiURL = "https://minelife-api.azurewebsites.net/";
 
-//Routes
-routes.get(`${apiURL}user_login`, userLogin);
+// //Routes
+// routes.get(`${apiURL}user_login`, userLogin);
 
 
 
-module.exports = routes;
+// module.exports = routes;
