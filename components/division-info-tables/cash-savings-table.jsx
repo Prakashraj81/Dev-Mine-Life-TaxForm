e4a4413ecf0c7e1cdeb1b719c8a9f9977f0c106b
Prakashraj81 <prakash.raj@g-japan.com>
2024-05-09 18:57:16 +0530

    correction changes completed summary page table

--- components/division-info-tables/cash-savings-table.jsx
+++ components/division-info-tables/cash-savings-table.jsx
@@ -165,15 +165,15 @@ export default function CashSavingsTable({ heir_details_list }) {
           </Alert>
         </Snackbar>
       </>
-      <div className="py-2">
+      <div className="">
         <Table aria-label="collapsible table">
           <TableHead className="table-head">
             <TableRow>
-              <TableCell className="border border-light-gray border-l" align="left"><span className="font-semibold">現金預金</span></TableCell>
-              <TableCell className="border border-light-gray border-l invisible" align="left"><span className="font-semibold">Column</span></TableCell>
-              <TableCell className="border border-light-gray border-l invisible" align="left"><span className="font-semibold">Column</span></TableCell>
-              <TableCell className="border border-light-gray border-l" align="right">{TotalAmount.toLocaleString()}<span className="inline-block float-right border-l text-right border-light-gray pl-1">円</span></TableCell>
-              <TableCell className="border border-light-gray border-l cursor-pointer" align="center" onClick={handleExpandFun}><span className="font-semibold">入力</span></TableCell>
+              <TableCell className="" align="left"><span className="font-semibold">現金預金</span></TableCell>
+              <TableCell className="invisible" align="left"><span className="font-semibold">Column</span></TableCell>
+              <TableCell className="invisible" align="left"><span className="font-semibold">Column</span></TableCell>
+              <TableCell className="" align="right">{TotalAmount.toLocaleString()}<span className="inline-block float-right border-l text-right border-light-gray pl-1">円</span></TableCell>
+              <TableCell className="cursor-pointer" align="center" onClick={handleExpandFun}><span className="font-semibold">入力</span></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
