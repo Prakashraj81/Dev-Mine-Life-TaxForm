170874bf35bb2508b81809f35b794284cf5c8ad9
Prakashraj81 <prakash.raj@g-japan.com>
2024-06-24 11:28:27 +0530

    Material UI added

--- components/layouts/full/FullLayout.tsx
+++ components/layouts/full/FullLayout.tsx
@@ -140,7 +140,7 @@ const FullLayout: React.FC<Props> = ({ children }) => {
                     </Box>
                   </Box>
                 </Box>
-                <Box className="w-20 hidden lg:inline-block xl:inline-block 2xl:inline-block float-left border-l sticky-sidebar">
+                <Box className="w-20 hidden lg:inline-block xl:inline-block 2xl:inline-block float-left border-l sticky-scroll">
                   <SideBarWidgetList RecentSaveList={RecentSaveList} />
                 </Box>
               </Box>
